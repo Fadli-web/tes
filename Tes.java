5ae181b59ea527e336220d990d5b62275b3c9468
Fadli-web <mohammad_fadli_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:15:26 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,9 @@
+ public class tes {
+     public static void main(string
+     []args)
+ } {
+     String nama ="fadli"
+     string kelas ="X RPL 5"
+     System.out.print("Halo,nama saya" + fadli + "dari kelas" + X RPL 5);
+ z
+ }
